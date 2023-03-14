0e0e5d35261240dc3d6bd8ff7ec403b9a18a176f
chiakaitu <chiakaitu860914@gmail.com>
2023-03-14 21:46:22 +0800

    Form create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 import { MainPageComponent } from './main-page/main-page.component';
 import { WeddingFlowComponent } from './wedding-flow/wedding-flow.component';
 import { MapInfoComponent } from './map-info/map-info.component';
 import { NoticeComponent } from './notice/notice.component';
 import { MealComponent } from './meal/meal.component';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 
 @NgModule({
   declarations: [
@@ -22,8 +26,11 @@ import { MealComponent } from './meal/meal.component';
   ],
   imports: [
     BrowserModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore())
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    // provideFirebaseApp(() => initializeApp(environment.firebase)),
+    // provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/meal/meal.component.ts
+++ src/app/meal/meal.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from "@angular/fire/compat/firestore";
+
+interface SubmitForm {
+  name: string,
+  mainCource: string
+};
 
 @Component({
   selector: 'app-meal',
@@ -6,5 +13,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./meal.component.scss']
 })
 export class MealComponent {
+  // item$: Observable<SubmitForm[]>;
+  formData = this.store.collection('data').valueChanges() as Observable<SubmitForm[]>;
 
+  constructor(private store: AngularFirestore) {}
+  public name = '123123'; // 名字
+  public seafood = ''; // 關於海鮮
+  public mainCource = ''; // 主餐
+
+  public resetMainCource() {
+    this.mainCource = '';
+  }
+
+  public submitForm() {
+    // 檢查姓名有沒有填寫
+    if (this.name == '') {
+      alert('請輸入姓名！');
+      return;
+    }
+
+    // 定義要送出的物件
+    let req = {
+      name: this.name,
+      mainCource: this.mainCource
+    };
+
+    this.store.firestore.runTransaction(() => {
+      return this.store.collection('data').add(req);
+    });
+    alert('感謝你的填寫！');
+  }
 }
